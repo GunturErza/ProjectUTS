aad62e14ec8db57cf2e45375e592c884e44732b8
GunturErza <kadekguntur002@gmail.com>
2026-04-13 16:53:48 +0800

    Guntur Membuat Enqueue
    Add Node and QueueKlinik classes with enqueue method

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+class Node:
+  def _init_(self, nama):
+    self.nama = nama
+    self.next = None
 
+class QueueKlinik:
+  def _init_(self):
+    self.front = None
+    self.rear = None
+
+# Enqueue: Menambahkan Antrian
+def enqueue(self, nama):
+  new_node = Node(nama)
+  if self.rear is None:
+    self.front = self.rear = new_node
+    printf(f"Pasien {nama} berhasil masuk antrian.")
+      return
+    self.rear.next = new_node
+    self.rear = new_node
+    print(f"Pasien {nama} berhasil masuk antrian.")
